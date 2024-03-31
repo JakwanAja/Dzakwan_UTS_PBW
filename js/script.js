056299239e104a4b4558f2f5cba64c6510689dc4
JakwanAja <muhzakwanalfaris195@gmail.com>
2024-03-31 22:16:00 +0700

    update

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var daftarButton = document.querySelector('.daftar_button');
+
+daftarButton.addEventListener('click', function () {
+    Swal.fire({
+        icon: 'success',
+        title: 'Sipp, Kamu Berhasil Mendaftar'
+    });
+});
